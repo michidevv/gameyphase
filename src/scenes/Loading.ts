267d5289b74547427e2da999c33c90f5c1eeb352
michidevv <michi.devv@gmail.com>
2023-07-15 09:37:48 +0200

    feat: simple loading

--- src/scenes/Loading.ts
+++ src/scenes/Loading.ts
@@ -1,3 +1,5 @@
+import { Text } from "../ui/Text";
+import { getTranslation } from "../utils/i18n";
 import { SCENES } from "./scenes";
 
 export class LoadingScene extends Phaser.Scene {
@@ -6,6 +8,15 @@ export class LoadingScene extends Phaser.Scene {
   }
 
   preload() {
+    new Text(
+      this,
+      this.game.scale.width / 2,
+      this.game.scale.height * 0.2,
+      getTranslation("loading"),
+      { fontSize: 28 }
+    )
+      .setAlign("center")
+      .setOrigin(0.5);
     this.load.baseURL = "./assets/";
     this.load.image({
       key: "tiles",
